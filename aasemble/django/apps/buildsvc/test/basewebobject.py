49b4f019be84ae85ac41daa64d69c130efa14227
Jaspreet Walia <walia.jaspreet@gmail.com>
2015-12-26 13:18:49 +0530

    just for testing local code

--- aasemble/django/apps/buildsvc/test/basewebobject.py
+++ aasemble/django/apps/buildsvc/test/basewebobject.py
@@ -1,6 +1,6 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-
 import subprocess
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium.webdriver.firefox.webdriver import WebDriver
 
